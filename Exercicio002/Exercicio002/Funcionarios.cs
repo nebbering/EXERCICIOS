134413df4aa2a7c4816739530de53e9420315941
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-03-20 15:32:34 -0300

    Up
    up

---
+++ Exercicio002/Exercicio002/Funcionarios.cs
@@ -0,0 +1,9 @@
+﻿namespace Exercicio002
+{
+    class Funcionarios
+    {
+        public string Nome;
+        public string Funcao;
+        public string Setor;
+    }
+}
---
+++ Exercicio002/Exercicio002/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Exercicio002;
+
+namespace Exercicio002
+{
+    class Metodos
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios fun1, fun2, fun3;
+
+            fun1 = new Funcionarios();
+            fun2 = new Funcionarios();
+            fun3 = new Funcionarios();
+
+            Console.WriteLine("Digite o primeiro nome do funcionário: ");
+            fun1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite a função: ");
+            fun1.Funcao = Console.ReadLine();
+            Console.WriteLine("Digite o setor do funcionário: ");
+            fun1.Setor = Console.ReadLine();
+
+            Console.WriteLine("Nome: " + fun1.Nome + "\n" + "Função: " + fun1.Funcao + "\n" + "Setor: " + fun1.Setor);
+
+        }
+    }
+}
