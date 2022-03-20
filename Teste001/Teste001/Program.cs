d65af65c85f31736f834d663f05d8306a2351b3a
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-03-20 11:26:14 -0300

    update
    up

---
+++ Teste001/Teste001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aplicacao
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite dois numeros");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int s = (x + y);
+            Console.WriteLine("O valor é: " + s);
+        }
+    }
+}
