4327d197fb0d271919cee59f34abfbdc167d170f
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-03-20 10:01:33 -0300

    New
    new

---
+++ exercicio01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso {
+    class Exercicio01 {
+        static void Main(string[] args)
+        {
+            int x = 10, y = 5;
+            int soma = (x + y);
+            System.Console.WriteLine("O resultado é: " + soma);
+        }
+    }
+}
